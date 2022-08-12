0f731ff78379f5c3a0cd805d05e049b02eabaa88
Mahimai Raja J <mahimairaja3@gmail.com>
2022-08-12 22:10:22 +0530

    Day 01 f string formats

---
+++ DAY_01/main.py
@@ -0,0 +1,9 @@
+x = 20
+
+print(f'{x:b}')      # binary      --> 10100
+
+print(f'{x:o}')      # octal       --> 24
+
+print(f'{x:x}')      # hexadecimal --> 14
+
+print(f'{x:f}')      # Float       --> 20.000000
